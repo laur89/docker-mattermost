ca26c02c393fd1480ef63e2ee8f7266fb995be91
Laur Aliste <laur.aliste@williamhill.com>
2020-09-25 14:04:29 +0200

    rework mattermost service file

--- mattermost.sh
+++ mattermost.sh
@@ -2,29 +2,17 @@
 
 readonly LOG_TIMESTAMP_FORMAT='+%Y-%m-%d %H:%M'
 
-MM_CONFIG=${MM_CONFIG:-/mattermost/config/config.json}
-readonly MM_EXEC=/opt/mattermost/bin/mattermost
-
-LOG="$(jq -r '.LogSettings.FileLocation' "$MM_CONFIG")"
-[[ -z "$LOG" || "$LOG" == null ]] && LOG=/mattermost/logs
-LOG+='/stdout.log'
+readonly MM_CONFIG=${MM_CONFIG:-/mattermost/config/config.json}
+readonly MM_EXEC=${MM_EXEC:-/opt/mattermost/bin/mattermost}
 
 
 wait_for_db() {
-    local db_host db_port
+    local db_host_and_port db_host db_port
 
-    __parse_db_connection_details() {
-        local db_host_and_port config
+    db_host_and_port="$(grep -Po '^\s*"DataSource":.*@tcp\(\K\w+:\d+(?=\).*$)' "$MM_CONFIG")" || fail "grepping db data from config file [$MM_CONFIG] failed"
+    IFS=':' read -r db_host db_port <<< "$db_host_and_port"
+    [[ -z "$db_host" || ! "$db_port" =~ ^[0-9]+$ ]] && fail "couldn't parse db hostname and/or port"
 
-        readonly config='/mattermost/config/config.json'
-
-        [[ -f "$config" ]] || fail "[$config] is not a valid file"
-        db_host_and_port="$(grep -Po '^\s*"DataSource":.*@tcp\(\K\w+:\d+(?=\).*$)' "$config")" || fail "grepping db data from config file [$config] failed"
-        IFS=':' read -r db_host db_port <<< "$db_host_and_port"
-        [[ -z "$db_host" || -z "$db_port" ]] && fail "couldn't parse db hostname and/or port"
-    }
-
-    __parse_db_connection_details
     log "Wait until database [$db_host:$db_port] is ready..."
     until nc -z "$db_host" "$db_port"; do
         sleep 2
@@ -35,6 +23,7 @@ wait_for_db() {
     return 0
 }
 
+# TODO: here trapping is not really necessary as MM is running in foreground via exec
 stop_server() {
     pgrep -f mattermost | xargs kill
     exit 0
@@ -49,7 +38,7 @@ fail() {
 log() {
     local msg
     readonly msg="$1"
-    echo -e "[$(date "$LOG_TIMESTAMP_FORMAT")]\tINFO  $msg" | tee --append "$LOG"
+    echo -e "[$(date "$LOG_TIMESTAMP_FORMAT")]\tINFO  $msg" | tee -a "$LOG"
     return 0
 }
 
@@ -58,12 +47,19 @@ err() {
     local msg
     readonly msg="$1"
     echo -e "\n\n    ERROR: $msg\n\n"  # do not pipe to $LOG
-    echo -e "[$(date "$LOG_TIMESTAMP_FORMAT")]\t    ERROR  $msg" | tee --append "$LOG"
+    echo -e "[$(date "$LOG_TIMESTAMP_FORMAT")]\t    ERROR  $msg" | tee -a "$LOG"
 }
 
 trap stop_server SIGINT SIGTERM
 
+[[ -f "$MM_CONFIG" ]] || fail "[$MM_CONFIG] is not a valid file"
 [[ "$AUTOSTART" =~ ^[Tt]rue$ && -x "$MM_EXEC" ]] || exit 0
+
+# define logfile:
+LOG="$(jq -r '.LogSettings.FileLocation' "$MM_CONFIG")"
+[[ $? -ne 0 || -z "$LOG" || "$LOG" == null ]] && LOG=/mattermost/logs
+LOG+='/stdout.log'
+
 
 wait_for_db
 
